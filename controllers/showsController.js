13796e0b9c5bf28c6ea0fe389243b953d76a24a5
Christian Capil <206409567@LA1-0015198.tfayd.com>
2014-10-23 18:22:14 -0700

    Added basic stub controller and view for shows
    Added controller, view, and javascript file for shows index
    Updated jquery.lazyload.js plugin to latest version to fix intermittent
    loading issues with requirejs

---
+++ controllers/showsController.js
@@ -0,0 +1,33 @@
+(function (showsController) {
+
+    var ravendb = require("../data/ravendb");
+    var data = require("../data");
+    var async = require('async');
+    var _ = require('extend');
+
+    showsController.init = function(app) {
+
+        app.get('/shows', function(req, res) {
+
+            var basePageModel = require("../models/page.js");
+            var model = new basePageModel({ title: 'Esquire Network Television Shows'}, req);
+            var homeObjects = _(true, homeObjects, model.getDefault);
+
+            homeObjects["shows"] = function (callback) {
+
+                data.getFeatured(function (err, results){
+                    callback(null, results);
+                });   
+                
+            };                
+
+            async.parallel(homeObjects, function(err, results){
+               if(!err) {
+                   model = _(true, model, results);
+                   console.log(model.shows);
+                   res.render('./shows/index', model);
+               } 
+            });
+        });
+    };
+})(module.exports);
---
+++ public/app/views/showsIndex.js
@@ -0,0 +1,8 @@
+require(['../../lib/common'], function (common){
+	require(['require'], function(require){
+
+		console.log("hello");
+
+	});
+});
+
